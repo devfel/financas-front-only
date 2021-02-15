2bc412bca0458898be26818224ebf3a9278f6353
Felizardo <devfelizardo@gmail.com>
2021-02-15 14:19:57 -0300

    adding form script code

--- index.js
+++ index.js
@@ -140,3 +140,54 @@ const Utils = {
     return signal + value;
   },
 };
+
+const Form = {
+  description: document.querySelector("input#description"),
+  amount: document.querySelector("input#amount"),
+  date: document.querySelector("input#date"),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
+
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
+    if (description.trim() === "" || amount.trim() === "" || date.trim() === "") {
+      throw new Error("Por favor, preencha todos os campos");
+    }
+  },
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+    amount = Utils.formatAmount(amount);
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date,
+    };
+  },
+
+  clearFields() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+  },
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      //Form.validateFields()
+      const transaction = Form.formatValues();
+      Transaction.add(transaction);
+      Form.clearFields();
+      modal.close();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+};
